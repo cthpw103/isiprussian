aefc14fb2711affb2903cfa8ac869de0d3937289
cth103 <cthdev@email.com>
2017-10-08 23:20:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ oReq.onload = function(e) {
 }
 oReq.send();
 var lines = database.split('\n');
-var ip = "8.8.8.8";
+var ip = "a russian ip";
 var withcheese = '';
+var russian = false;
 for(var i = 0;i < lines.length;i++){
     withcheese = lines[i].split(',')
-    for(var e = 0;e < withcheese.length;e++){
-        //help
-        if (withcheese[1].split('.')[e] = ip.split('.')[1]) {
-            if (withcheese[2].split('.')[e] = ip.split('.')[2]) {
-                alert("is russian");
-            }
-        }
+    if (withcheese[1].split('.')[1].toString() = ip.split('.')[1]) {
+       russian = true;
     }
 }
+return russian;
