69083aac968284468bfa3c2566333389754c9353
cth103 <cthdev@email.com>
2017-10-08 23:08:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var lines = database.split('\n');
+var ip = "8.8.8.8";
 var withcheese = '';
 for(var i = 0;i < lines.length;i++){
     withcheese = lines[i].split(',')
     for(var e = 0;e < withcheese.length;i++){
         //help
-        withcheese[e].split('.')[1]
+        if (withcheese[e].split('.')[1] = ip.split('.')[1]) {
+            alert("probably russian");
+            if (withcheese[e].split('.')[2] = ip.split('.')[2]) {
+                alert("is russian :)");
+            }
+        }
     }
 }
