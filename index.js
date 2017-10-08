50eff7d0a866297d7f5c41d6a06451f29a77e2d8
cth103 <cthdev@email.com>
2017-10-08 23:24:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ for(var i = 0;i < lines.length;i++){
     withcheese = lines[i].split(',')
     if (withcheese[1].split('.')[1].toString() = ip.split('.')[1]) {
        russian = true;
+    } else if (withcheese[1].split('.')[2].toString() = ip.split('.')[2]) {
+       russian = true;
     }
 }
 return russian;
