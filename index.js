20de9f8d5801f434e952aca5c69f5d16041e0556
cth103 <cthdev@email.com>
2017-10-08 23:05:02 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var lines = database.split('\n');
+var withcheese = '';
+for(var i = 0;i < lines.length;i++){
+    withcheese = lines[i].split(',')
+    for(var e = 0;e < withcheese.length;i++){
+        //help
+        withcheese[e].split('.')[1]
+    }
+}
