7d60a95dc8ec496b8712271da6eb62d07cd057e0
cth103 <cthdev@email.com>
2017-10-08 23:11:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var ip = "8.8.8.8";
 var withcheese = '';
 for(var i = 0;i < lines.length;i++){
     withcheese = lines[i].split(',')
-    for(var e = 0;e < withcheese.length;i++){
+    for(var e = 0;e < withcheese.length;e++){
         //help
         if (withcheese[e].split('.')[1] = ip.split('.')[1]) {
-            alert("probably russian");
             if (withcheese[e].split('.')[2] = ip.split('.')[2]) {
-                alert("is russian :)");
+                alert("is russian");
             }
         }
     }
