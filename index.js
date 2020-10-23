72d3cff155770b004dcf4f825ff83d68483df56b
Alan <alanrd772@gmail.com>
2020-10-23 21:55:07 +0400

    switched to using api

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var oReq = new XMLHttpRequest();
-var database;
-oReq.open("GET", "database", true);
-oReq.onload = function(e) {
-   database = oReq.responseText; 
+const http = require("http")
+const ip = "89.250.165.81"; // example russian ip
+
+let options = {
+   host: 'ip-api.com',
+   path: `/json/${ip}`
+};
+
+callback = function(response) {
+   var str = '';
+ 
+   // another chunk of data has been received, so append it to `str`
+   response.on('data', function (chunk) {
+      str += chunk;
+   });
+ 
+   // the whole response has been received, so we just print it out here
+   response.on('end', function () {
+      if (JSON.parse(str).country == "Russia") console.log("Russian hacker detected")
+      else console.log("Network is safe")
+   });
 }
-oReq.send();
-var lines = database.split('\n');
-var ip = "a russian ip";
-var withcheese = '';
-var russian = false;
-for(var i = 0;i < lines.length;i++){
-    withcheese = lines[i].split(',')
-    if (withcheese[1].split('.')[1].toString() = ip.split('.')[1]) {
-       russian = true;
-    } else if (withcheese[1].split('.')[2].toString() = ip.split('.')[2]) {
-       russian = true;
-    }
-}
-return russian;
+ 
+http.request(options, callback).end();
