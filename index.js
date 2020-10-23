74c24056525193b0be82291a1986f8026cec8b4d
Alan <alanrd772@gmail.com>
2020-10-23 23:49:59 +0400

    Added extra info and package.json

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const http = require("http")
-const ip = "89.250.165.81"; // example russian ip
+const ip = "89.250.165.81"; // example russian IP
+// 89.20.165.81 works to test for non-russian IP.
 
-let options = {
+let options = { // http request options
    host: 'ip-api.com',
    path: `/json/${ip}`
 };
 
 callback = function(response) {
-   var str = '';
- 
-   // another chunk of data has been received, so append it to `str`
+   let str = '';
+   // a chunk of data has been received, so append it to `str`
    response.on('data', function (chunk) {
       str += chunk;
    });
  
-   // the whole response has been received, so we just print it out here
+   // the whole response has been received, so we handle the result
    response.on('end', function () {
-      if (JSON.parse(str).country == "Russia") console.log("Russian hacker detected")
-      else console.log("Network is safe")
+      let country = JSON.parse(str).country;
+      if (country == "Russia") console.log(`Russian hacker detected at IP address ${ip}`)
+      else console.log(`Network is safe, IP origin is ${country}`)
    });
 }
  
